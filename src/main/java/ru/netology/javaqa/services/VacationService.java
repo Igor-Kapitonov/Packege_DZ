2e562003b557da7a26e1615ba55f800e8655caf9
Igor Kapitonov <kizero182@gmail.com>
2024-02-25 15:09:18 +0300

    Initial commit

---
+++ src/main/java/ru/netology/javaqa/services/VacationService.java
@@ -0,0 +1,21 @@
+package ru.netology.javaqa.services;
+
+public class VacationService {
+
+    public int calculate (int income, int expenses, int threshold){
+        int months =0;
+        int cash =0;
+        for (int i =0; i<=12; i++){
+            if (cash >= threshold){ // идем в отпуск
+                months++;
+                cash -= expenses;
+                cash = cash / 3;
+                }
+            else {  // работаем
+                cash += income;
+                cash -= expenses;
+            }
+        }
+        return months;
+    }
+}
---
+++ src/test/java/ru/netology/javaqa/services/VacationServiceTest.java
@@ -0,0 +1,25 @@
+package ru.netology.javaqa.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class VacationServiceTest {
+    @Test
+    public void tast() {
+        VacationService service = new VacationService();
+
+        int expected = 3;
+        int actual = service.calculate(10_000, 3_000, 20_000);
+        Assertions.assertEquals(expected, actual);
+    }
+        @Test
+        public void test() {
+            VacationService service = new VacationService();
+
+            int expected =3;
+            int actual = service.calculate(100_000, 60_000, 150_000);
+            Assertions.assertEquals(expected, actual);
+
+        }
+
+}
