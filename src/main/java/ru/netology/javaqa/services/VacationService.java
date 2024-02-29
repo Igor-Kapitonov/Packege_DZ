4334aa5cfc4487822b310323dd9940c5109c2779
Igor Kapitonov <kizero182@gmail.com>
2024-02-29 19:35:34 +0300

    Reformat code

--- src/main/java/ru/netology/javaqa/services/VacationService.java
+++ src/main/java/ru/netology/javaqa/services/VacationService.java
@@ -2,16 +2,15 @@ package ru.netology.javaqa.services;
 
 public class VacationService {
 
-    public int calculate (int income, int expenses, int threshold){
-        int months =0;
-        int cash =0;
-        for (int i =0; i<=12; i++){
-            if (cash >= threshold){ // идем в отпуск
+    public int calculate(int income, int expenses, int threshold) {
+        int months = 0;
+        int cash = 0;
+        for (int i = 0; i <= 12; i++) {
+            if (cash >= threshold) { // идем в отпуск
                 months++;
                 cash -= expenses;
                 cash = cash / 3;
-                }
-            else {  // работаем
+            } else {  // работаем
                 cash += income;
                 cash -= expenses;
             }
--- src/test/java/ru/netology/javaqa/services/VacationServiceTest.java
+++ src/test/java/ru/netology/javaqa/services/VacationServiceTest.java
@@ -12,14 +12,15 @@ public class VacationServiceTest {
         int actual = service.calculate(10_000, 3_000, 20_000);
         Assertions.assertEquals(expected, actual);
     }
-        @Test
-        public void test() {
-            VacationService service = new VacationService();
 
-            int expected =3;
-            int actual = service.calculate(100_000, 60_000, 150_000);
-            Assertions.assertEquals(expected, actual);
+    @Test
+    public void test() {
+        VacationService service = new VacationService();
 
-        }
+        int expected = 3;
+        int actual = service.calculate(100_000, 60_000, 150_000);
+        Assertions.assertEquals(expected, actual);
+
+    }
 
 }
